0f0c749431943e80a9eba7fa7b35348afc1824c5
arnaudjuracek <arnaud.juracek+github@gmail.com>
2015-09-02 18:40:27 +0200

    Add comments & README.md

---
+++ site/plugins/iftttbot/README.md
@@ -0,0 +1,17 @@
+# IFTTT BOT plugin
+
+A plugin for [Kirby CMS](http://getkirby.com) to automatically post from a make request. Designed for IFTTT's Make channel.
+
+## Installation
+### IFTTT side
+### Kirby side
+
+## Usage
+
+
+## Authors
+Louis Eveillard & Arnaud Juracek
+
+## Changelog
+
+* **1.0.0** Initial release
--- site/plugins/iftttbot/iftttbot.php
+++ site/plugins/iftttbot/iftttbot.php
@@ -3,77 +3,139 @@
 
 /**
  * IFTTT BOT plugin
- * v 0.1
- * c::set('routes', array(
- *   array(
- *       'pattern' => 'bot/add/',
- *       'action'  => function() {
+ * v 1.0.0
+ * Louis Eveillard & Arnaud Juracek
  *
- *       }
- *   ),
+ *
+ * config.php :
+ *
+ *
+ * c::set('routes', array(
+ *	array(
+ *		'pattern' => md5(site()->url()) . '/(:any)/(:any)',
+ *		'action'  => function($page, $blueprint){
+ *			return create_post($page, $blueprint, $_POST);
+ *		},
+ *		'method' => 'POST'
+ *	),
+ *
+ *	array(
+ *		'pattern' => 'md5',
+ *		'action'  => function(){
+ *			echo site()->url() . '<br>';
+ *			echo md5(site()->url());
+ *		},
+ *	),
  * ));
  *
  */
 
+
+
+// Where the magic happens
+
 function create_post($page, $blueprint, $data){
+
+	// Where we'll put the content.
 	$PATH = get_content_path($page);
 	$SLUG = str::slug($data["title"]);
+		$dir = $PATH . DS . $SLUG;
+		$dir_matches = glob($PATH . DS . "*" . $SLUG . "*");
 
-	$dir = $PATH . DS . $SLUG;
-	$dir_matches = glob($PATH . DS . "*" . $SLUG . "*");
-
+	// If the directory already exists don't override it,
+	// append a number to it, no matter its visibility.
+		// 1-directory
+		// directory_1
+		// 8-directory_2
 	if(count($dir_matches) > 0){
 		$dir .= "_" . count($dir_matches);
 		$data['title'] .= "_" . count($dir_matches);
 	}
 
+	// Create the directory with read&write permissions.
 	mkdir($dir, 0777, true);
 
+	// Filename with (almost) multilingual support.
+	// Peraphs you'll want to create different files for each
+	// languages code.
 	$filename = $blueprint . ".fr.txt";
 
+	// Write the file.
 	$file = fopen($dir . DS . $filename, 'w');
 	if(flock($file, LOCK_EX)) {
 		fwrite($file, parse_data(get_blueprint($blueprint), $data));
 		flock($file, LOCK_EX);
 	}
 	fclose($file);
+
 }
 
+
+
+// Create the text file for the article
+
 function parse_data($blueprint, $data){
+
+	// This is the separator for markdown file.
 	$separator = "\n\n----\n\n";
 
+	// Append each param in the IFTTT body form in the markdown file,
+	// separated with the markdown separator.
+	// If a param name doesn't match the blueprint, it will be deleted
+	// next time you save the article in the panel.
 	$output = "";
 	foreach($blueprint['fields'] as $key => $value){
 		$output .= $separator;
 		$output .= ucfirst($key) . ": " . $data[$key];
 	}
 
+	// Trim the first and last separator.
 	$output = trim($output, $separator);
 
 	return $output;
+
 }
 
 
+
+// Get the path of the content/page
+
 function get_content_path($page){
+
+	// Get the content path...
 	$path = kirby()->roots()->content() . DS;
+
+	// ... no matter its visibility ( 1-project is the same as project)
 	$dir = glob($path . "*" . $page);
 
 	if(count($dir)==1) return $dir[0];
 	else return false;
+
 }
 
 
+
+// Get the blueprint of the content/page/blueprint
+
 function get_blueprint_path($blueprint){
+
 	$path = kirby()->roots->blueprints() . DS;
 	$file = glob($path . $blueprint . '.php');
 
 	if(count($file)==1) return $file[0];
 	else return false;
+
 }
 
+
+
+// Return an array containing all field in the blueprint
+
 function get_blueprint($blueprint){
+
 	$file = get_blueprint_path($blueprint);
 
 	if($file) return yaml::read($file);
 	else return false;
+
 }
