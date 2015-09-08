4258497ed1b7c7e7cd14070a43a5cf875cce991d
arnaudjuracek <arnaud.juracek+github@gmail.com>
2015-09-08 13:03:25 +0200

    Add a description

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,5 +1,6 @@
 <header class="main" role="banner">
 	<a class="title" href="<?php echo url() ?>"><?php echo $site->title() ?></a>
+	<div class="description"><div class="content"><?php echo $site->description() ?></div></div>
 	<?php if(param('tag')): ?>
 		<span class="tag"><?php echo "#" . urldecode(param('tag')) ?></span>
 	<?php endif ?>
