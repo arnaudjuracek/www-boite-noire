c2423a4b68db6ba2ad9ef23686f2ade64aab9bcc
arnaudjuracek <arnaud.juracek+github@gmail.com>
2015-09-09 21:09:20 +0200

    Change header template

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,10 +1,12 @@
 <header class="main" role="banner">
-	<a class="title" href="<?php echo url() ?>"><?php echo $site->title() ?></a>
-	<div class="description"><div class="content"><?php echo $site->description() ?></div></div>
+	<a class="title" href="<?php echo url() ?>"><?php echo $site->title() ?>
 	<?php if(param('tag')): ?>
 		<span class="tag"><?php echo "#" . urldecode(param('tag')) ?></span>
-	<?php endif ?>
-	<?php if($user = site()->user() and $user->hasPanelAccess()): ?>
-		<a class="button" href="<?php echo $site->url() . '/panel/#/pages/add/blog' ?>" target="_blank">+</a>
-	<?php endif ?>
+	<?php endif ?></a>
+	<div class="description"><div class="content"><?php echo $site->description() ?></div></div>
+
+	<aside class="io">
+		<div id="input" title="input">i<span class="autohide">nput</span>&ensp;&mdash;</div>
+		<div id="output" title="output">&mdash;&ensp;o<span class="autohide">utput</span></div>
+	</aside>
 </header>
